787ecd2adf0bd8b4e41cec1e03ec34f0afcaf525
Waichak Luk <waichak.luk@gmail.com>
2024-11-10 04:09:08 -0800

    Updating the tasks

--- RunAreaReportUpdate.py
+++ RunAreaReportUpdate.py
@@ -2,10 +2,10 @@ import requests
 import os
 
 url = f"https://www.drillbreaker29.com/soldhomes/areareport"
-
+url2 = f"https://www.drillbreaker29.com/maintanance/updatelistings"
 payload = {}
 headers = {}
 
+response = requests.request("POST", url2, headers=headers, data={'doz': '1095'})
 response = requests.request("PATCH", url, headers=headers, data=payload)
-
 print(response.text)
