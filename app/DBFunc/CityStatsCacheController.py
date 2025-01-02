9c1390f269cdfa54611cbf909738dbce01c58166
Waichak Luk <waichak.luk@gmail.com>
2025-01-02 14:26:18 -0800

    Missing a commit from before.

---
+++ app/DBFunc/CityStatsCacheController.py
@@ -0,0 +1,57 @@
+from app.DBModels.CityStatsCache import CityStatsCache
+from sqlalchemy.sql import func
+from app.extensions import db
+from datetime import datetime, timedelta
+from app.config import Config
+import pytz
+
+class CityStatsCacheController:
+    def __init__(self):
+        self.db = db
+        self.CityStatsCache = CityStatsCache
+
+    def get_all_city_stats(self):
+        """Retrieve all city statistics from the cache."""
+        return self.db.session.query(self.CityStatsCache).all()
+
+    def update_city_stats(self, city_name, sold, pending, forsale, updated_time):
+        """Update or insert a city's stats in the cache."""
+        city_stats = self.db.session.query(self.CityStatsCache).filter_by(city_name=city_name).first()
+
+        if city_stats is None:
+            # Create new entry
+            city_stats = CityStatsCache(city_name=city_name)
+
+        # Update values
+        city_stats.sold = sold
+        city_stats.pending = pending
+        city_stats.forsale = forsale
+        city_stats.updated_time = updated_time
+
+        # Add or update in the session
+        self.db.session.add(city_stats)
+        self.db.session.commit()
+
+    def refresh_city_stats(self, cities, brieflistingcontroller):
+        """Refresh city stats for a list of cities."""
+
+
+        for city in cities:
+            print(city)
+            try:
+                updated_time = datetime.fromtimestamp(
+                    brieflistingcontroller.latestListingTime(city)
+                )
+            except:
+                updated_time = None
+
+
+            self.update_city_stats(
+                city_name=city,
+                sold=brieflistingcontroller.soldListingsByCity(city, 30),
+                pending=brieflistingcontroller.pendingListingsByCity(city, 30),
+                forsale=brieflistingcontroller.forSaleListingsByCity(city, 365),
+                updated_time=updated_time
+            )
+
+citystatscachecontroller = CityStatsCacheController()
